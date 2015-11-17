da6ac824b750d29c00f2d2b0a9397782ba65d84c
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-11-17 18:52:36 +0200

    Adds composer.json

---
+++ languages/fi.php
@@ -0,0 +1,53 @@
+<?php
+
+return array(
+	'tour:title:edit' => 'Editing contents for: %s',
+	'tour:no_results' => 'No results',
+	'tour:stops:none' => "There aren't yet any tour stops defined for this page. Click 'Add new tour stop' to add the first stop.",
+	'tour:start' => 'Help',
+	'tour:not_found' => 'The item was not found',
+	'tour:add' => 'Add new tour page',
+	'tour:stop:add' => 'Add new tour stop',
+	'tour:page:title' => 'Tour for the page: %s',
+
+	// Action words for the tour steps
+	'done' => 'Sulje',
+	'skip' => 'Ohita',
+
+	'tour:error:page_not_found' => 'The selected page is missing or invalid',
+
+	'tour:action:save:success' => 'Tour stop saved',
+	'tour:action:reorder:success' => 'Reordered %s items',
+	'tour:action:delete:success' => 'Deleted',
+	'tour:action:delete:error' => 'Failed to delete the item',
+
+	'tour:target' => 'Target element',
+	'tour:target:desc' => 'Enter class or id of the target element starting with the hash or the period character. For example <b>.elgg-menu-item-blog</b>. (If you are using the Joyride library, you can leave this empty to display general information.)',
+	'tour:page' => 'Page',
+	'tour:page:desc' => 'Select the page specific tour that this stop will be added to.',
+	'tour:placement' => 'Placement',
+	'tour:left' => 'Left',
+	'tour:bottom' => 'Bottom',
+	'tour:right' => 'Right',
+	'tour:top' => 'Top',
+
+	'admin:administer_utilities:tour' => 'Feature tours',
+	'admin:administer_utilities:tour:view' => 'Feature tour',
+	'admin:administer_utilities:tour:add' => 'Add a new feature tour',
+	'admin:administer_utilities:tour:edit' => 'Edit feature tour',
+	'admin:administer_utilities:tour:stop:add' => 'Add a new feature tour stop',
+	'admin:administer_utilities:tour:stop:edit' => 'Edit feature tour stop',
+
+	'item:object:tour_page' => 'Tour pages',
+	'item:object:tour_stop' => 'Tour stops',
+
+	'tour:context:1' => 'Only this page',
+	'tour:context:2' => 'Only subpages of this page',
+	'tour:context:3' => 'This page and all of its subpages',
+	'tour:context:4' => 'All pages',
+
+	'tour:setting:library' => 'Javascript library',
+	'tour:setting:library:desc' => 'There are two tour libraries to choose from:
+<a href="http://zurb.com/playground/jquery-joyride-feature-tour-plugin">Joyride</a> and
+<a href="http://linkedin.github.io/hopscotch/">Hopscotch</a>',
+);
